55e5b8242113366f0b755386395b24024327f876
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-23 12:54:25 -0300

    feat: create and iplement About

---
+++ src/components/About.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full mt-12 mb-12 items-center flex justify-center'>
+        <div className='w-[60vw] gap-y-10'>
+        <h1 className='text-4xl font-bold'>Sobre a Handyman</h1>
+      <p className='mt-10 text-xl text-justify'>
+      A plataforma **Handyman** é uma solução digital inovadora que conecta clientes a profissionais de serviços gerais 
+      de forma prática, rápida e eficiente. Projetada para atender às demandas de um mercado crescente, a Handyman 
+      oferece uma interface amigável que facilita tanto a contratação quanto a oferta de serviços. Com apenas alguns 
+      cliques, o cliente pode encontrar profissionais qualificados em diversas áreas, como reparos domésticos, 
+      encanamento, eletricidade, pintura, jardinagem, manutenção de eletrodomésticos, entre outros.
+      Uma das grandes vantagens da plataforma é a sua capacidade de filtrar e recomendar prestadores de serviços com base 
+      na localização do usuário, avaliação de clientes anteriores e disponibilidade imediata. Isso garante que o cliente 
+      tenha acesso aos melhores profissionais da sua região, promovendo a confiança e a qualidade nos serviços contratados.</p>
+        <p className='text-justify text-xl'>
+      Além disso, a Handyman oferece recursos de comunicação integrados, como chat em tempo real, para que os usuários possam 
+      esclarecer dúvidas e negociar detalhes antes de fechar um acordo. A segurança é uma prioridade, com processos de 
+      verificação de identidade para os profissionais cadastrados e sistemas de pagamento seguros e transparentes.
+      Para os prestadores de serviços, a Handyman funciona como uma excelente vitrine digital, permitindo que eles alcancem 
+      novos clientes sem a necessidade de grandes investimentos em marketing. A plataforma também oferece ferramentas de 
+      gerenciamento, como agenda de compromissos e histórico de atendimentos, para que os profissionais possam organizar 
+      suas atividades de maneira eficiente.
+     Outro diferencial da Handyman é o sistema de avaliação e feedback, que valoriza os profissionais de destaque e ajuda a 
+     melhorar continuamente a experiência do usuário. Com foco na qualidade e na conveniência, a plataforma está 
+     transformando a forma como as pessoas contratam e oferecem serviços.
+     Seja você um cliente que precisa de ajuda para resolver problemas domésticos ou um profissional em busca de 
+     oportunidades, a Handyman é a solução ideal para conectar necessidades a habilidades, tornando o dia a dia mais 
+     prático e produtivo.
+      </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
